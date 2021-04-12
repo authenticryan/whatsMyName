84d391e9ca2387e4cfb8b4e1201207116af3ba0e
Ryan Dsouza <34348029+authenticryan@users.noreply.github.com>
2021-04-12 15:59:40 +0530

    Data set added, and totalBirths Method added into main java class

--- WhatsMyName/WhatsMyName.java
+++ WhatsMyName/WhatsMyName.java
@@ -1,4 +1,5 @@
-
+import edu.duke.*;
+import org.apache.commons.csv.*;
 /**
  * Write a description of WhatsMyName here.
  * 
@@ -8,5 +9,25 @@
 
 
 public class WhatsMyName {
+    public void totalBirths(FileResource fr) {
+        int totalFemaleBirths = 0, totalMaleBirths = 0, totalBirths = 0;
 
+        for (CSVRecord record : fr.getCSVParser(false)) {
+            
+            if (record.get(1).equals("F")){
+                totalFemaleBirths += Integer.parseInt(record.get(2));
+            } else {
+                totalMaleBirths += Integer.parseInt(record.get(2));
+            }   
+        }
+        totalBirths = totalFemaleBirths + totalMaleBirths;
+        System.out.println("Total Births is " + totalBirths +
+                            "\nTotal Female Births is " + totalFemaleBirths +
+                            "\nTotal Male Births is " + totalMaleBirths);
+    }    
+    
+    public void testTotalBirths() {
+        FileResource fr = new FileResource("../Data Sets/us_babynames_test/yob2012short.csv");
+        totalBirths(fr);
+    }
 }
