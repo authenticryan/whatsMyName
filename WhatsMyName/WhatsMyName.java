6463c87c1676e6512fad090faa64baea3e1020fa
Ryan Dsouza <34348029+authenticryan@users.noreply.github.com>
2021-04-12 19:58:46 +0530

    getName() and its test function ready
    Get name returns name of the individual, at corresponding ranking in Gender

--- WhatsMyName/WhatsMyName.java
+++ WhatsMyName/WhatsMyName.java
@@ -34,7 +34,7 @@ public class WhatsMyName {
     }
     
     // returns rank of a given name, from a given year. Returns -1 if no name is availabe in that year
-    public int getRank(String searchYear, String searchName, String searchGender) {
+    public int getRank(int searchYear, String searchName, String searchGender) {
         FileResource fr = new FileResource("../Data Sets/us_babynames_by_year/yob"+searchYear+".csv");
         int index = 0;
         for (CSVRecord record : fr.getCSVParser(false)) {
@@ -50,18 +50,51 @@ public class WhatsMyName {
     }
     
     public void testGetRank() {
-        int testRank = getRank("1930", "Myriam", "F");
+        int testRank = getRank(1930, "Myriam", "F");
         System.out.println("Expected result for Myrian, Female in 1930 -> 3934. getRank() Function Returned -> " + testRank);
         
-        testRank = getRank("1930", "Zoma", "F");
+        testRank = getRank(1930, "Zoma", "F");
         System.out.println("Expected result for Zoma, Female in 1930 -> 5248. getRank() Function Returned -> " + testRank);
         
         
-        testRank = getRank("1930", "Robert", "M");
+        testRank = getRank(1930, "Robert", "M");
         System.out.println("Expected result for Robert, Male in 1930 -> 1. getRank() Function Returned -> " + testRank);
         
-        testRank = getRank("1930", "Json", "M");
+        testRank = getRank(1930, "Json", "M");
         System.out.println("Expected result for Json, Male in 1930 -> -1. getRank() Function Returned -> " + testRank);
 
+    }
+    
+    // return name of the individual, at corresponding ranking in Gender 
+    public String getName(int searchYear, int searchRank, String searchGender){
+        FileResource fr = new FileResource("../Data Sets/us_babynames_by_year/yob"+searchYear+".csv");
+        int index = 0;
+        
+        for (CSVRecord record : fr.getCSVParser(false)) {
+            if(record.get(1).equalsIgnoreCase(searchGender)) {
+                index++;
+                if(index == searchRank)
+                    return record.get(0);
+            }
+        }
+        return "No Name Found";
+    }
+    
+    public void testGetName() {
+        String testName;
+        
+        System.out.println("Beginning testing getName()\n");
+        
+        testName = getName(1930, 12, "F");
+        System.out.println("Expected Virginia, Female at 12th Rank. Received -> " + testName);
+        
+        
+        testName = getName(1930, 5248, "F");
+        System.out.println("Expected Zoma, Female at 5248th Rank. Received -> " + testName);
+        
+        
+        testName = getName(1930, 12, "M");
+        System.out.println("Expected Paul, Male at 12th Rank. Received -> " + testName);
+        
     }
 }
