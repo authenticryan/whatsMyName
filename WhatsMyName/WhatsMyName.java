63736f75321be15a52608ef1a0f05418cb064cf4
Ryan Dsouza <34348029+authenticryan@users.noreply.github.com>
2021-04-13 08:43:03 +0530

    yearOfHighestRank and testYearOfHighestRank are built
    return the year, where searchName was ranked highest. Returns -1 if searchName was not found in any files. Lower the rank, the better.

--- WhatsMyName/WhatsMyName.java
+++ WhatsMyName/WhatsMyName.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import edu.duke.*;
 import org.apache.commons.csv.*;
 /**
@@ -33,9 +34,8 @@ public class WhatsMyName {
         System.out.println("\nExpected output - Total Births - 73. Female Births - 40, Male Births - 33");
     }
     
-    // returns rank of a given name, from a given year. Returns -1 if no name is availabe in that year
-    public int getRank(int searchYear, String searchName, String searchGender) {
-        FileResource fr = new FileResource("../Data Sets/us_babynames_by_year/yob"+searchYear+".csv");
+    // returns rank of a given name, from a given FileResource. Returns -1 if no name is availabe in that year
+    public int getRankLogic(FileResource fr, String searchName, String searchGender) {
         int index = 0;
         for (CSVRecord record : fr.getCSVParser(false)) {
             if(record.get(1).equalsIgnoreCase(searchGender)) {
@@ -45,8 +45,13 @@ public class WhatsMyName {
                 }
             }
         }
-        
         return -1;
+    }
+    
+    // returns rank of a given name, from a given year. Returns -1 if no name is availabe in that year
+    public int getRank(int searchYear, String searchName, String searchGender) {
+        FileResource fr = new FileResource("../Data Sets/us_babynames_by_year/yob"+searchYear+".csv");
+        return getRankLogic(fr, searchName, searchGender);
     }
     
     public void testGetRank() {
@@ -95,6 +100,84 @@ public class WhatsMyName {
         
         testName = getName(1930, 12, "M");
         System.out.println("Expected Paul, Male at 12th Rank. Received -> " + testName);
+        
+    }
+    
+    public void whatIsNameInYear(String birthName, int birthYear, int newYear, String gender) {
+        int rankAtBirthYear = getRank(birthYear, birthName, gender);
+        String nameAtNewYear = null;
+        if (rankAtBirthYear != -1) {
+            nameAtNewYear = getName(newYear, rankAtBirthYear, gender);
+            if(!nameAtNewYear.equals("No Name Found"))
+                System.out.println(birthName + " born in " + birthYear + " would be " + nameAtNewYear + " if he was born in " + newYear);
+            else 
+                System.out.println("Rank Out of Bounds");
+        } else {
+            System.out.println("Birth Name not available in BirthYear Dataset");
+        }
+    }
+    
+    public void testWhatIsNameInYear() {
+        whatIsNameInYear("Ryan", 1997, 1930, "M");
+        System.out.println("Expected Raymond\n");
+        
+        whatIsNameInYear("Jennifer", 1997, 1930, "F");
+        System.out.println("Expected Joyce\n");
+        
+        
+        whatIsNameInYear("Not A name", 1997, 1930, "M");
+        System.out.println("Expected error indicating birth name was not available in birth year dataset\n");
+                
+        whatIsNameInYear("Zykerria", 1997, 1930, "F");
+        System.out.println("Expected error indicating similar name not available in the new year dataset\n");
+    }
+    
+    // return the year, where searchName was ranked highest. Returns -1 if searchName was not found in any files. Lower the rank, the better.
+    public int yearOfHighestRank(String searchName, String searchGender) {
+        int lowestRank = -1, yearOfHighestRank = 0;
+        String fileWithHighestRank = null;
+        
+        DirectoryResource dr = new DirectoryResource();
+        for(File f : dr.selectedFiles()) {
+            FileResource fr = new FileResource(f);
+            int currentRank = getRankLogic(fr, searchName, searchGender);
+            if (currentRank != -1) {
+                if (lowestRank == -1) {
+                    lowestRank = currentRank;
+                    fileWithHighestRank = f.getName();
+                } 
+                if (currentRank < lowestRank) {
+                    lowestRank = currentRank;
+                    fileWithHighestRank = f.getName();
+                }
+            }
+        }
+        
+        if (fileWithHighestRank != null) 
+            yearOfHighestRank = Integer.parseInt(fileWithHighestRank.substring(3,7));
+        else 
+            yearOfHighestRank = -1;
+            
+        return yearOfHighestRank;
+    }
+    
+    public void testYearOfHighestRank() {
+        System.out.println("Plesae open files yob2012short.csv till yob2014short.csv from whatsMyName\\Data Sets\\us_babynames_test");
+        
+        // test for male
+        System.out.println("Running test for Mason, Male");
+        int testOutcome = yearOfHighestRank("Mason", "M");
+        System.out.println("Expected Outome -> 2012. Returned -> " + testOutcome);
+
+        // test for female
+        System.out.println("Running test for Olivia, Female");
+        testOutcome = yearOfHighestRank("Olivia", "F");
+        System.out.println("Expected Outome -> 2014. Returned -> " + testOutcome);        
+        
+        // test for non existing name
+        System.out.println("Running test for Ryan, Male");
+        testOutcome = yearOfHighestRank("Ryan", "M");
+        System.out.println("Expected Outome -> -1. Returned -> " + testOutcome);
         
     }
 }
